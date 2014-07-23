7c2dd1d36de61ffedff32dd298fc7371b2482cac
Thomas Harris <tom.harris@just-eat.com>
2014-07-23 08:34:59 +0100

    Change install directory

--- OWINTest.Service/uninstall.cmd
+++ OWINTest.Service/uninstall.cmd
@@ -1,5 +1,5 @@
 path = %path%;C:\Windows\Microsoft.NET\Framework\v4.0.30319;
 
-installutil /u C:\_Personal_Dev\OWIN-OpenRasta-Service\OWINTest.Service\bin\Debug\OpenRasta.Owin.Service.exe
+installutil /u C:\_JE_Dev\openrasta-hosting-owin\OWINTest.Service\bin\Debug\OpenRasta.Owin.Service.exe
 
 pause
